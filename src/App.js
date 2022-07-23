40b993740ce90c96ccf051e1f15188bedff48e72
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-07-23 11:09:16 +0100

    Add list function.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [lists, setLists] = useState([]);
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleAddClick = () => {
+    setLists([...lists, text]);
+    console.log([...lists, text]);
+    setText("");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Todo</h1>
+
+        <h2>Title</h2>
+        <input
+          type="text"
+          className=""
+          value={text}
+          onChange={handleTextChange}
+        />
+        <button onClick={handleAddClick}>Add</button>
+        <div className="App-content">
+          <ul>
+            {lists.map((list, id) => {
+              console.log(id);
+              return <li key={id}>{list}</li>;
+            })}
+          </ul>
+        </div>
       </header>
     </div>
   );
